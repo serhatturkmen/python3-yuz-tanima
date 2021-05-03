bf959704bfc41176b915c2a75d491a6fe570a377
serhatturkmen <serhat_21_88@hotmail.com>
2021-05-03 13:21:10 +0300

    firstcommit

--- README.md
+++
@@ -1,7 +0,0 @@
-# python3-yuz-tanima
-Python yüz tanıma
-
-# kullanılan kütüphaneler
-+ numpy
-+ cv2
-+ face_recognition
--- yuztanima.py
+++ yuztanima.py
@@ -2,33 +2,29 @@
 import face_recognition
 import cv2
 import numpy as np
+import os
 
+folder = os.getcwd()
 
 # opencv metodu olan VideoCapture ile webcam'den görüntü almayı başlatıyoruz // 0 default webcam video_capture = cv2.VideoCapture(0)
 video_capture = cv2.VideoCapture(0)
 
-# Yukarıdaki "mennan sevim" resmini yüklüyoruz ve encoding bilgisini alıyoruz
+# Yukarıdaki tanınan resmini yüklüyoruz ve encoding bilgisini alıyoruz
 serhat_image = face_recognition.load_image_file(
-    "/home/serhat/Desktop/profil1.jpg")
+    folder + "/profil1.jpg")
 serhat_face_encoding = face_recognition.face_encodings(serhat_image)[0]
 
-# Yukarıdaki "miray sevim" resmini yüklüyoruz ve encoding bilgisini alıyoruz
+# Yukarıdaki tanınmayan resmini yüklüyoruz ve encoding bilgisini alıyoruz
 bilinmeyen_image = face_recognition.load_image_file(
-    "/home/serhat/Desktop/profil2.jpg")
+    folder + "/profil2.jpg")
 bilinmeyen_face_encoding = face_recognition.face_encodings(bilinmeyen_image)[0]
 
 # Encoding ve açıklama kısmını burada tanımlıyoruz, birden fazla tanımlayabiliriz
 known_face_encodings = [serhat_face_encoding, bilinmeyen_face_encoding]
 known_face_names = ["Serhat", "Yeni Kişi"]
 
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
-
-
 while True:
-    # Videodan anlık bir kare yakalıyoruz
+    # Grab a single frame of video
     ret, frame = video_capture.read()
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
@@ -43,37 +39,34 @@ while True:
         # See if the face is a match for the known face(s)
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
 
-        # Tanımlı olmayan yüzlere ne yazılacak
-        name = "Bilinmiyor"
+        name = "Unknown"
 
+        # If a match was found in known_face_encodings, just use the first one.
+        # if True in matches:
+        #     first_match_index = matches.index(True)
+        #     name = known_face_names[first_match_index]
+
+        # Or instead, use the known face with the smallest distance to the new face
         face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
         best_match_index = np.argmin(face_distances)
         if matches[best_match_index]:
             name = known_face_names[best_match_index]
 
-        # Sonuçları göster
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            top += 25
-            right += 10
-            bottom += 15
-            left += 10
-
-        # Yüzü kırmızı çerçeve ile göster
+        # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Yüzün bilgilerini gir
-        cv2.rectangle(frame, (left, bottom + 30), (right, bottom), (0, 0, 255), cv2.FILLED)
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom + 25), font, 0.7, (255, 255, 255), 1)
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-    # Ekranda göster
-    cv2.imshow('Yüz tanımlama sistemi', frame)
+    # Display the resulting image
+    cv2.imshow('Video', frame)
 
-    # q tuşuna basıldığı zaman çıkış yap
+    # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-
-# Kamerayı kapat
+# Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
